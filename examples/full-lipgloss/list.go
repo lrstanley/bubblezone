b723e5c0b1af0e2afdde7d094b72892a8b05fe29
Liam Stanley <liam@liam.sh>
2024-06-09 13:16:05 -0400

    docs: update license refs to reference new email
    Signed-off-by: Liam Stanley <liam@liam.sh>

--- examples/full-lipgloss/list.go
+++ examples/full-lipgloss/list.go
@@ -1,5 +1,5 @@
-// Copyright (c) Liam Stanley <me@liamstanley.io>. All rights reserved. Use
-// of this source code is governed by the MIT license that can be found in
+// Copyright (c) Liam Stanley <liam@liam.sh>. All rights reserved. Use of
+// this source code is governed by the MIT license that can be found in
 // the LICENSE file.
 
 package main
